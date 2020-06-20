c72f0cadf531dbfd4c6d07297e266ec812764c01
rajashekar <minumularajashekar@gmail.com>
2020-06-20 18:30:48 +0530

    Commit 4  R file

--- R_bike.R
+++ R_bike.R
@@ -16,6 +16,7 @@ lapply(x, require, character.only = TRUE)
 #commit1 addes on R file
 #commit2 added on R file
 #commit3 added on R file
+#commit4 added on R file
  
 #missing values
 missing_val=data.frame(apply(bike_data,2,function(x){sum(is.na(x))}))
