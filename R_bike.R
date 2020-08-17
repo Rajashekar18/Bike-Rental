ee3aa438d0bc57efc74c98d60f23d50d92625211
Minumula Rajashekar <rajashekarminumula94@gmail.com>
2020-08-17 17:34:49 +0530

    Update R_bike.R
    missing value new comment

--- R_bike.R
+++ R_bike.R
@@ -18,7 +18,7 @@ lapply(x, require, character.only = TRUE)
 #commit3 added on R file
 #commit4 added on R file
  #Edit commit
-#missing values
+#missing values new comment
 missing_val=data.frame(apply(bike_data,2,function(x){sum(is.na(x))}))
 missing_val
 missing_val[2]=colnames(bike_data)
